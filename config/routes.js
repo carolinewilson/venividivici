7b2a20698039856691369e32a78499bd720d8d47
carolinewilson <carolinecwilson3+github@gmail.com>
2016-11-16 14:29:39 +0000

    Adding secure routes

--- config/routes.js
+++ config/routes.js
@@ -2,27 +2,28 @@ const router = require('express').Router();
 const authController = require('../controllers/auth');
 const users = require('../controllers/users');
 const locations = require('../controllers/locations');
+const secureRoute = require('../lib/secureRoute');
 
 router
   .post('/login', authController.login)
   .post('/register', authController.register);
 
 router.route('/users')
-  .get(users.index)
+  .get(secureRoute, users.index)
   .post(users.create);
 
 router.route('/users/:id')
-  .get(users.show)
-  .put(users.update)
-  .delete(users.delete);
+  .get(secureRoute, users.show)
+  .put(secureRoute, users.update)
+  .delete(secureRoute, users.delete);
 
 router.route('/locations')
   .get(locations.index)
-  .post(locations.create);
+  .post(secureRoute, locations.create);
 
 router.route('/locations/:id')
   .get(locations.show)
-  .put(locations.update)
-  .delete(locations.delete);
+  .put(secureRoute, locations.update)
+  .delete(secureRoute, locations.delete);
 
 module.exports = router;
