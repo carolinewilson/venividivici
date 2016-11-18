f554ffe6ae6efd49cb00139e09fd45f47e14bae3
carolinewilson <carolinecwilson3+github@gmail.com>
2016-11-18 15:43:13 +0000

    Added progress bar to budget tracker

--- src/js/controllers/budgetTracker.js
+++ src/js/controllers/budgetTracker.js
@@ -7,6 +7,15 @@ function BudgetTrackerController(Trip, $state) {
   const budgetTracker = this;
 
   budgetTracker.trip = Trip.get($state.params);
+  budgetTracker.pcSaved = (budgetTracker.trip.totalSavings / (budgetTracker.trip.flightCost + budgetTracker.trip.expenses + budgetTracker.trip.accomCost)) * 100;
+
+  function calcPcSaved() {
+
+    budgetTracker.pcSaved = (budgetTracker.trip.totalSavings / (budgetTracker.trip.flightCost + budgetTracker.trip.expenses + budgetTracker.trip.accomCost)) * 100;
+
+    return Math.ceil(budgetTracker.pcSaved);
+  }
+
 
   function save() {
     budgetTracker.trip.$update((data) => {
@@ -15,4 +24,5 @@ function BudgetTrackerController(Trip, $state) {
   }
 
   budgetTracker.save = save;
+  budgetTracker.calcPcSaved = calcPcSaved;
 }
