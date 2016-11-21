3a1ed9a06c328f13faeb109e72bdaf5448eb5130
carolinewilson <carolinecwilson3+github@gmail.com>
2016-11-21 13:01:32 +0000

    Allowing creation of location in db WITH user ID

--- src/js/controllers/locationsForms.js
+++ src/js/controllers/locationsForms.js
@@ -3,23 +3,29 @@ angular.module('travelApp')
   .controller('LocationsNewController', LocationsNewController);
 
 //////new
-LocationsNewController.$inject = ['Location', '$state'];
+LocationsNewController.$inject = ['Location', '$state', '$auth'];
 
-function LocationsNewController(Location, $state) {
+function LocationsNewController(Location, $state, $auth) {
   const locationsNew = this;
   locationsNew.location = {};
 
   function createLocation() {
+    // set airport info
     const airport = locationsNew.location.airport.split(',');
     locationsNew.location.closestAirport = airport[0];
     locationsNew.location.airportCode = airport[1];
 
+    // push images into array
     locationsNew.location.images = [locationsNew.location.tempImage.one, locationsNew.location.tempImage.two, locationsNew.location.tempImage.three, locationsNew.location.tempImage.four, locationsNew.location.tempImage.five];
-    
+
+    // get userId from paylod
+    locationsNew.location.user = $auth.getPayload()._id;
+
     console.log(locationsNew.location);
-    // Location.save(locationsNew.location, () => {
-    //   $state.go('home');
-    // });
+    // Save location
+    Location.save(locationsNew.location, () => {
+      $state.go('home');
+    });
   }
   locationsNew.createLocation = createLocation;
 }
