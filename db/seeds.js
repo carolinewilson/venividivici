25a9920460b8a0aa95f0ce7a739a57cb71eb16af
miss-coder10 <dara.dc10@gmail.com>
2016-11-22 20:33:45 +0000

    with updated profile pic

--- db/seeds.js
+++ db/seeds.js
@@ -23,7 +23,7 @@ User.create([{
   username: 'Dara',
   email: 'dara@gmail.com',
   preferredAirport: 'LGW',
-  profilePic: 'http://orig15.deviantart.net/3a07/f/2009/343/2/8/el_conquistador_by_wredwrat.jpg',
+  profilePic: '../../images/kittyme.jpg',
   password: 'password' ,
   passwordConfirmation: 'password'
 },{
